8a1e4054bd44cfe1089b543076da237c99566c7d
Valencia Starr <valenciastarr@Valencias-MacBook-Pro.local>
2023-06-20 13:10:21 -0400

    crud lab

---
+++ src/components/QuestionList.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function QuestionList() {
+  return (
+    <section>
+      <h1>Quiz Questions</h1>
+      <ul>{/* display QuestionItem components here after fetching */}</ul>
+    </section>
+  );
+}
+
+export default QuestionList;
