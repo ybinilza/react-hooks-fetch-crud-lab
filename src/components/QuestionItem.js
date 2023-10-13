ec099f46c0c9df438489baf0ffdaa53b97038946
Binil Thomas Zachariah <binilreni7@gmail.com>
2023-10-13 12:21:52 -0400

    Lab completed

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, handleDeletedQuestion }) {
   const { id, prompt, answers, correctIndex } = question;
 
   const options = answers.map((answer, index) => (
@@ -8,6 +8,14 @@ function QuestionItem({ question }) {
       {answer}
     </option>
   ));
+
+  function handleDelete(id) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE",
+    })
+      .then((resp) => resp.json())
+      .then(() => handleDeletedQuestion(id));
+  }
 
   return (
     <li>
@@ -17,7 +25,7 @@ function QuestionItem({ question }) {
         Correct Answer:
         <select defaultValue={correctIndex}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={() => handleDelete(id)}>Delete Question</button>
     </li>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+  const [questions, setQuestions] = useState([]);
+
+  function handleDeletedQuestion(id) {
+    const updatedQuestions = questions.filter((question) => {
+      return question.id != id;
+    });
+    setQuestions(updatedQuestions);
+  }
+
+  const questionList = questions.map((question) => {
+    return (
+      <QuestionItem
+        key={question.id}
+        question={question}
+        handleDeletedQuestion={handleDeletedQuestion}
+      ></QuestionItem>
+    );
+  });
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then((resp) => resp.json())
+      .then((question) => setQuestions(() => question));
+  }, []);
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{questionList}</ul>
     </section>
   );
 }
